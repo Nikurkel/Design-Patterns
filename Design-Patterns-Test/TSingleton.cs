fbcc484930e052eef9d09f7c6b104f7b0feca8ad
Nina <66831842+Nikurkel@users.noreply.github.com>
2023-06-06 20:42:44 +0200

    Singleton
    added Singleton pattern

---
+++ Design-Patterns-Test/TSingleton.cs
@@ -0,0 +1,44 @@
+﻿namespace Design_Patterns_Test
+{
+    using NUnit.Framework;
+    using Design_Patterns.Singleton;
+    using System.Threading.Tasks;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    [TestFixture]
+    public class TSingleton
+    {
+        [Test]
+        public void Instance_ShouldReturnSameInstance()
+        {
+            var firstInstance = Singleton.Instance;
+            var secondInstance = Singleton.Instance;
+
+            Assert.AreSame(firstInstance, secondInstance);
+        }
+
+        [Test]
+        public void Instance_ShouldReturnSameInstanceInMultiThreadedScenario()
+        {
+            var instances = new List<Singleton>();
+
+            Parallel.For(0, 100, _ =>
+            {
+                var instance = Singleton.Instance;
+                instances.Add(instance);
+            });
+
+            Assert.That(1, Is.EqualTo(instances.Distinct().Count()));
+        }
+
+        [Test]
+        public void GetInfo_ShouldReturnCorrectMessage()
+        {
+            var referenceInfo = "This is some Singleton information.";
+            var info = Singleton.Instance.GetInfo();
+
+            Assert.That(info, Is.EqualTo(referenceInfo));
+        }
+    }
+}
---
+++ Design-Patterns/Singleton/Singleton.cs
@@ -0,0 +1,35 @@
+﻿namespace Design_Patterns.Singleton
+{
+    public sealed class Singleton
+    {
+        private static Singleton instance = null;
+        private static readonly object lockObject = new object();
+
+        private Singleton()
+        {
+        }
+
+        public static Singleton Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock (lockObject)
+                    {
+                        if (instance == null)
+                        {
+                            instance = new Singleton();
+                        }
+                    }
+                }
+                return instance;
+            }
+        }
+
+        public string GetInfo()
+        {
+            return "This is some Singleton information.";
+        }
+    }
+}
